a81296929b2529eb3e9870518b9888c3c9918408
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-02-22 13:29:37 +0600

    added functionality on coupon and modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ let selectedSeats = 0;
 let totalPrice = 0;
 let grandPrice = 0;
 let perTicket = 550;
+let myCoupons = ["NEW15", "Couple 20"];
+let couponApplied = false;
 
 // update available seats
 function updateSeatCount() {
@@ -17,10 +19,8 @@ function updatePrice() {
     totalPrice += perTicket;
     grandPrice += perTicket;
     document.getElementById("total-price").innerText = totalPrice;
-    document.getElementById("grand-price").innerText = totalPrice;
+    document.getElementById("grand-price").innerText = grandPrice;
 }
-
-console.log(document.getElementById("available-seats").innerText);
 
 // update ticket list
 function updateTicket(element) {
@@ -31,12 +31,12 @@ function updateTicket(element) {
     console.log(tickets[selectedSeats].childNodes[1].classList);
 }
 
-// get seat plan
+// add event for selecting seats
 let allSeats = document.querySelectorAll(".seat-plan");
-console.log(allSeats);
+
 for (let i = 0; i < allSeats.length; i++) {
     allSeats[i].addEventListener("click", (e) => {
-        if (selectedSeats < 4) {            
+        if (selectedSeats < 4) {
             updatePrice();
             updateTicket(allSeats[i]);
             updateSeatCount();
@@ -46,11 +46,53 @@ for (let i = 0; i < allSeats.length; i++) {
     });
 }
 
-// tickets and price
-// let tickets = document.querySelectorAll(".selected-ticket");
-// for (let ticket of tickets) {
-//     console.log("----------", ticket, "--------------");
-//     let ticketId = ticket.childNodes[1].innerText;
-//     let ticketClass = ticket.childNodes[3].innerText;
-//     let ticketPrice = ticket.childNodes[5].innerText;
-// }
+function discount(percentage) {
+    couponApplied = true;
+    grandPrice -= percentage * grandPrice;
+    document.getElementById("grand-price").innerText = grandPrice;
+    document.getElementById("coupon-segment").classList.add("hidden");
+}
+
+// input coupon
+document.getElementById("coupon").addEventListener("keyup", (e) => {
+    let inputCoupon = e.target.value;
+    console.log(inputCoupon);
+    if (couponApplied === false && selectedSeats === 4) {
+        if (myCoupons.indexOf(inputCoupon) > -1) {
+            document
+                .getElementById("coupon-apply")
+                .classList.remove("btn-disabled");
+
+            console.log(inputCoupon);
+
+            document
+                .getElementById("coupon-apply")
+                .addEventListener("click", () => {
+                    let percentage = 0.15;
+                    if (!couponApplied) {
+                        if (inputCoupon === myCoupons[0]) {
+                            discount(percentage);
+                        } else if (inputCoupon === myCoupons[1]) {
+                            percentage = 0.2;
+                            discount(percentage);
+                        }
+                    }
+                });
+        }
+    }
+});
+
+// process next and show modal
+document.getElementById("btn-next").addEventListener("click", () => {
+    if (selectedSeats > 0) {
+        document.getElementById("page-header").classList.add("hidden");
+        document.getElementById("page-main").classList.add("hidden");
+        document.getElementById("page-footer").classList.add("hidden");
+        document.getElementById("page-modal").classList.remove("hidden");
+    }
+});
+
+// reload on continue
+document.getElementById("page-reload").addEventListener("click", () => {
+    location.reload();
+});
